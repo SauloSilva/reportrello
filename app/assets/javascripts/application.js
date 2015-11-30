706dd9f71dc7aa84a64a91fb914ca02d8b0bb2bd
Saulo Santiago <saulodasilvasantiago@gmail.com>
2015-11-30 01:13:27 -0200

    Add logic to get token of trello by url

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,14 @@
 //= require jquery
+
+$(function() {
+  $('.login-trello').click(function() {
+    Trello.authorize({
+      type: "redirect",
+      name: "Reportrello",
+      scope: {
+        read: true,
+        write: true
+      }
+    });
+  });
+})
