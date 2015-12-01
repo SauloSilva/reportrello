197b667ca677c5961c9c1d116fb86032bd2ef861
Saulo Santiago <saulodasilvasantiago@gmail.com>
2015-12-01 01:06:21 -0200

    Add JS to authentication and get user information by token

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,13 +2,50 @@
 
 $(function() {
   $('.login-trello').click(function() {
-    Trello.authorize({
-      type: "redirect",
-      name: "Reportrello",
-      scope: {
-        read: true,
-        write: true
-      }
-    });
+    Reportrello.authorize();
   });
+
+  Reportrello = {
+    authorize: function() {
+      var self = this
+
+      Trello.authorize({
+        type: 'popup',
+        name: 'Reportrello',
+        fragment: 'postmessage',
+        scope: {
+          read: true,
+          write: true
+        },
+        expiration: 'never',
+        success: function() {
+          self.getMe();
+        },
+        error: function() {
+          alert("Failed authentication");
+        }
+      });
+    },
+
+    getMe: function() {
+      var token = localStorage.getItem('trello_token');
+
+      Trello.rest(
+        'GET',
+        'members/me',
+        {
+          fields: 'username,fullName,avatar',
+          token: token
+        },
+        function (object) {
+          location.href = '/authentication/?user[token]=' + token + '&user[username]=' + object.username + '&user[fullname]=' + object.fullName
+        },
+        function (a) { alert("Failed authentication"); }
+      );
+    },
+
+    newReport: function() {
+
+    }
+  }
 })
