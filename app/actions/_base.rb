9b08d2d0848925f0a2b5d910b6345403fad88023
Saulo Santiago <saulodasilvasantiago@gmail.com>
2015-12-05 20:16:04 -0200

    Add register to auth and make action to loggout

--- app/actions/_base.rb
+++ app/actions/_base.rb
@@ -1,5 +1,28 @@
-def errors_handler(record)
-  record.errors.map do |key, value|
-    flash.now["error_#{ key }".to_sym] = "#{ key } #{ value }"
+register do
+  def auth(has_authentication)
+    condition do
+      return unless has_authentication
+
+      unless user_logged?
+        flash[:error] = 'You is not logged'
+        redirect '/'
+      end
+    end
   end
+end
+
+helpers do
+  def errors_handler(record)
+    record.errors.map do |key, value|
+      flash.now["error_#{ key }".to_sym] = "#{ key } #{ value }"
+    end
+  end
+
+  def user_logged?
+    @current_user.present?
+  end
+end
+
+before do
+  @current_user ||= User.find_by(token: session[:user_token])
 end
--- app/actions/authentication.rb
+++ app/actions/authentication.rb
@@ -2,10 +2,19 @@ get '/authentication/' do
   user = User.find_or_initialize_by(token: params[:user][:token])
 
   if user.update_attributes params[:user]
-    flash.now[:success] = 'Logged in with successfully'
-    redirect "user/#{ user.id }/imports/new"
+    session[:user_token] = user.token
+
+    flash[:success] = 'Logged in with successfully'
+    redirect '/reports/new'
   else
     errors_handler(user)
     haml :'home/index'
   end
+end
+
+get '/loggout', auth: true do
+  session[:user_token] = nil
+
+  flash[:success] = 'Loggout with successfully'
+  redirect '/'
 end
