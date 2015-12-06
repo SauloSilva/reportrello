97fb382f94186723c15bb6f3ccd744fa50021dee
Saulo Santiago <saulodasilvasantiago@gmail.com>
2015-12-06 18:12:30 -0200

    Add method warning? and success? to model report and dependet destroy to tasks relation

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,8 +5,16 @@ class Report < ActiveRecord::Base
   validates :description, length: { in: 6..255 }
 
   belongs_to :user
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 
   scope :not_reported, -> { where(reported_at: nil) }
   scope :by_user, -> (user_id) { where(user_id: user_id) }
+
+  def warning?
+    status == 'warning'
+  end
+
+  def success?
+    status == 'success'
+  end
 end
